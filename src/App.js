5f4a381d57d424ca3404853f75f639cb4c6d0d81
Dez Beberniss <dezmarie@gmail.com>
2021-12-03 17:36:50 -0800

    Initial list component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 
 import ClassCounter from './components/ClassCounter';
 import HooksCounter from './components/HooksCounter';
+import ReducerList from './components/ReducerList';
 
 function App() {
   return (
     <div className="App">
       <ClassCounter max={25} step={5} />
+      <hr />
       <HooksCounter max={25} step={5} />
+      <hr />
+      <ReducerList />
     </div>
   );
 }
